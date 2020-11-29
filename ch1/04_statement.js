187c112d9ee75569612a4c7bf18f32de4758b3fe
jinwoongBang <dlopo123@gmail.com>
2020-11-29 23:08:10 +0900

    순서 변경

--- ch1/04_statement.js
+++ ch1/04_statement.js
@@ -1,4 +1,16 @@
 function statement(invoice, plays) {
+  let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
+  for (let perf of invoice.performances) {
+    result += `${playFor(perf).name} : ${usd(amountFor(perf))} (${
+      perf.audience
+    }석)\n`;
+  }
+
+  result += `총액 : ${usd(totalAmount())}\n`;
+  result += `적립 포인트 : ${totalVolumeCredits()}점\n`;
+
+  return result;
+
   function totalAmount() {
     let result = 0;
     for (let perf of invoice.performances) {
@@ -59,18 +71,6 @@ function statement(invoice, plays) {
 
     return result;
   }
-
-  let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
-  for (let perf of invoice.performances) {
-    result += `${playFor(perf).name} : ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
-  }
-
-  result += `총액 : ${usd(totalAmount())}\n`;
-  result += `적립 포인트 : ${totalVolumeCredits()}점\n`;
-
-  return result;
 }
 
 module.exports = statement;

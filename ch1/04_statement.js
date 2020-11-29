ac04619bf6f28fc7e4c3a34d042ac8e241a907e1
jinwoongBang <dlopo123@gmail.com>
2020-11-29 22:46:09 +0900

    변수명 변경

--- ch1/04_statement.js
+++ ch1/04_statement.js
@@ -1,11 +1,11 @@
 function statement(invoice, plays) {
-  function volumeCreditsFor(perf) {
+  function volumeCreditsFor(aPerformance) {
     let volumeCredits = 0;
     // 포인트를 적립한다
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
     // 희극 관객 5명 마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(aPerformance).type)
+      volumeCredits += Math.floor(aPerformance.audience / 5);
 
     return volumeCredits;
   }

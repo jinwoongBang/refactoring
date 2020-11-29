b48c6af12667af8ea68c45be18999ab8d476615c
jinwoongBang <dlopo123@gmail.com>
2020-11-29 22:24:29 +0900

    [1] 중첩 함수의 추가

--- ch1/index.js
+++ ch1/index.js
@@ -1,6 +1,6 @@
 const invoicesData = require("./invoices.json");
 const playsData = require("./plays.json");
-const statement = require("./statement.js");
+const statement = require("./02_statement.js");
 
 for (let invoice of invoicesData) {
   console.log(statement(invoice, playsData));

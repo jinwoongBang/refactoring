b69286f654c4565d66f311df208cdfd2d68c3e2d
jinwoongBang <dlopo123@gmail.com>
2020-11-29 22:57:40 +0900

    totalVolumeCredits 함수로 추출

--- ch1/04_statement.js
+++ ch1/04_statement.js
@@ -1,4 +1,12 @@
 function statement(invoice, plays) {
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+  }
+
   function usd(aNumber) {
     return new Intl.NumberFormat("en-us", {
       style: "currency",
@@ -54,11 +62,8 @@ function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
-  
+  let volumeCredits = totalVolumeCredits();
+
   result += `총액 : ${usd(totalAmount)}\n`;
   result += `적립 포인트 : ${volumeCredits}점\n`;
 

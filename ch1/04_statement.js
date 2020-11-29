e51b30b75af368fa5a09556c7b1dd622bdbf8e5e
jinwoongBang <dlopo123@gmail.com>
2020-11-29 23:55:46 +0900

    데이터 처리하는 로직 분리

--- ch1/04_statement.js
+++ ch1/04_statement.js
@@ -1,10 +1,14 @@
 function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
+}
+
+function createStatementData(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
   statementData.totalAmount = totalAmount(statementData);
   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData, plays);
+  return statementData;
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
@@ -53,15 +57,11 @@ function statement(invoice, plays) {
   }
 
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `청구 내역 (고객명 : ${data.customer})\n`;
   for (let perf of data.performances) {
     result += `${perf.play.name} : ${usd(perf.amount)} (${perf.audience}석)\n`;

3d2628231f8fa1352d5994eaafddc70c0e0a4304
jinwoongBang <dlopo123@gmail.com>
2020-11-29 23:01:13 +0900

    반복문 쪼개기 및 문장 슬라이드 하기

--- ch1/04_statement.js
+++ ch1/04_statement.js
@@ -52,13 +52,15 @@ function statement(invoice, plays) {
     return result;
   }
 
-  let totalAmount = 0;
   let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
-
   for (let perf of invoice.performances) {
     result += `${playFor(perf).name} : ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
+  }
+  
+  let totalAmount = 0;
+  for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
 

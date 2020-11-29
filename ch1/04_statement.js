bd88335a0656aea48de5fe18d3013dfae3eb08b2
jinwoongBang <dlopo123@gmail.com>
2020-11-29 23:31:07 +0900

    playFor 함수를 statement 로 옮김

--- ch1/04_statement.js
+++ ch1/04_statement.js
@@ -6,14 +6,19 @@ function statement(invoice, plays) {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
     return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
 
 function renderPlainText(data, plays) {
   let result = `청구 내역 (고객명 : ${data.customer})\n`;
   for (let perf of data.performances) {
-    result += `${playFor(perf).name} : ${usd(amountFor(perf))} (${
+    result += `${perf.play.name} : ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
   }
@@ -52,19 +57,15 @@ function renderPlainText(data, plays) {
     // 포인트를 적립한다
     result += Math.max(aPerformance.audience - 30, 0);
     // 희극 관객 5명 마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(aPerformance).type)
+    if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
 
     return result;
   }
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
   function amountFor(aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -78,7 +79,7 @@ function renderPlainText(data, plays) {
         }
         break;
       default:
-        throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르 : ${aPerformance.play.type}`);
     }
 
     return result;

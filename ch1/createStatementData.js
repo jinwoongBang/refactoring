07cbee8eb7b735ea52dd5448343f105d8905a27a
jinwoongBang <dlopo123@gmail.com>
2020-11-30 21:49:09 +0900

    팩터리 패턴을 통해 타입에 맞는 객체 생성

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -3,8 +3,18 @@ module.exports = {
 };
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
 }
+
+class TragedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {}
 
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {

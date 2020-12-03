76d7fd3660cd199049bde1493737150dd6e2c172
jinwoongBang <dlopo123@gmail.com>
2020-12-03 22:46:48 +0900

    이동된 파일 삭제

--- ch1/index.js
+++
@@ -1,8 +0,0 @@
-const invoicesData = require("./invoices.json");
-const playsData = require("./plays.json");
-const { statement, htmlStatement } = require("./04_statement.js");
-
-for (let invoice of invoicesData) {
-  // console.log(statement(invoice, playsData));
-  console.log(htmlStatement(invoice, playsData));
-}

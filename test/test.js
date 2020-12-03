b7ae31be8c7f94dd3aa00925aefc536b20771a33
jinwoongBang <dlopo123@gmail.com>
2020-12-03 23:25:23 +0900

    chai 라이브러리 추가

--- test/test.js
+++ test/test.js
@@ -1,11 +1,21 @@
 // const assert = require("assert");
 // const { sampleProvinceData, Province } = require("../src/ch4/Province");
 import assert from "assert";
+import chai from "chai";
 import { sampleProvinceData, Province } from "../src/ch4/Province.js";
+
+const expect = chai.expect;
 
 describe("province", function () {
   it("shortfall", function () {
     const asia = new Province(sampleProvinceData()); // [1] 픽스쳐 설정 (고정장치)
     assert.equal(asia.shortfall, 5); // [2] 검증
+  });
+});
+
+describe("province", function () {
+  it("shortfall", function () {
+    const asia = new Province(sampleProvinceData()); // [1] 픽스쳐 설정 (고정장치)
+    expect(asia.shortfall).equal(5);
   });
 });

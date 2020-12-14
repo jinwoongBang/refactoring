8a3414635ee395d1658a89c58347ffedf8244f95
jinwoongBang <dlopo123@gmail.com>
2020-12-14 22:57:19 +0900

    printBanner 함수 추출

--- src/ch6/01_extractFunction/index.js
+++ src/ch6/01_extractFunction/index.js
@@ -1,9 +1,8 @@
 // [before refactoring]
 function printOwing(invoice) {
   let outstanding = 0;
-  console.log("*****************");
-  console.log("*****고객 채무*****");
-  console.log("*****************");
+
+  printBanner();
 
   // 미해결 채무(outstanding) 를 계산한다.
   for (const o of invoice.orders) {
@@ -11,7 +10,7 @@ function printOwing(invoice) {
   }
 
   // 마감일(dueDate) 를 기록한다.
-  const today = Clock.today;
+  const today = new Date();
   invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
@@ -22,4 +21,15 @@ function printOwing(invoice) {
   console.log(`고객명: ${invoice.customer}`);
   console.log(`채무액: ${outstanding}`);
   console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+
+  function printBanner() {
+    console.log("*****************");
+    console.log("*****고객 채무*****");
+    console.log("*****************");
+  }
 }
+
+printOwing({
+  customer: "김철수",
+  orders: [{ amount: 10 }, { amount: 20 }, { amount: 30 }],
+});

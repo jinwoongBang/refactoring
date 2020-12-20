0cd567fccf8feced8fa72a1960d74f780f6afd5a
jinwoongBang <dlopo123@gmail.com>
2020-12-20 23:41:37 +0900

    [175페이지] 변수 추출하기 - 클래스 형태 예제

--- src/ch6/03_extractVariable/index.js
+++ src/ch6/03_extractVariable/index.js
@@ -9,3 +9,24 @@ function price(order) {
 
   return basePrice - quantityDiscount + shipping;
 }
+
+class Order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get quantity() {
+    return this._data.quantity;
+  }
+  get itemPrice() {
+    return this._data.itemPrice;
+  }
+
+  get price() {
+    return (
+      this.quantity * this.itemPrice -
+      Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+      Math.min(basePrice * 0.1, 100)
+    );
+  }
+}

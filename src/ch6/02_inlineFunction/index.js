b0e4d1bb82b506d84e377dfda8c879b601c0de1f
jinwoongBang <dlopo123@gmail.com>
2020-12-26 20:33:14 +0900

    [6.11] 단계 쪼개기 예제

--- src/ch6/02_inlineFunction/index.js
+++ src/ch6/02_inlineFunction/index.js
@@ -1,6 +1,9 @@
 /**
  *
- * @deprecated{함수를 굳이 나눌 필요가 없을 때 Inline 하는 것이 좋다.}
+ * @deprecated{
+ *  함수를 굳이 나눌 필요가 없을 때 Inline 하는 것이 좋다.
+ *  1. 반대 리팩터링 : 함수 추출하기
+ * }
  */
 function getRating(driver) {
   return moreThanFiveLateDeliveries(driver) ? 2 : 1;

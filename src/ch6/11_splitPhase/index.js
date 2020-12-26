765567636b343fbeae9acb5e09dd802996f929c6
jinwoongBang <dlopo123@gmail.com>
2020-12-26 21:13:05 +0900

    [6.11] 4단계 가격 계산 로직 한번 더 쪼개기

--- src/ch6/11_splitPhase/index.js
+++ src/ch6/11_splitPhase/index.js
@@ -1,34 +1,27 @@
 function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  const price = applyShipping(priceData, shippingMethod);
+
+  return price;
+}
+
+function calculatePricingData(product, quantity) {
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const priceData = { basePrice };
-  const price = applyShipping(
-    priceData,
-    // basePrice,
-    shippingMethod,
-    quantity,
-    discount
-  );
 
-  return price;
+  return { basePrice, quantity, discount };
 }
 
-function applyShipping(
-  priceData,
-  //   basePrice,
-  shippingMethod,
-  quantity,
-  discount
-) {
+function applyShipping(priceData, shippingMethod) {
   const shippingPerCase =
     priceData.basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountFree
       : shippingMethod.freePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = priceData.basePrice - discount + shippingCost;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
 
   return price;
 }

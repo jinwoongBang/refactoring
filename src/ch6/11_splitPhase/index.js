06468750dc8ff7d025a13e697314c5b9aca83714
jinwoongBang <dlopo123@gmail.com>
2020-12-26 20:44:03 +0900

    [6.1] 단계 쪼개기 - 3단계 매개 변수 추가

--- src/ch6/11_splitPhase/index.js
+++ src/ch6/11_splitPhase/index.js
@@ -4,12 +4,25 @@ function priceOrder(product, quantity, shippingMethod) {
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const price = applyShipping();
+  const priceData = {};
+  const price = applyShipping(
+    priceData,
+    basePrice,
+    shippingMethod,
+    quantity,
+    discount
+  );
 
   return price;
 }
 
-function applyShipping(basePrice, shippingMethod, quantity, discount) {
+function applyShipping(
+  priceData,
+  basePrice,
+  shippingMethod,
+  quantity,
+  discount
+) {
   const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountFree

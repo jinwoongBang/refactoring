cc36b3a1d3b5a2b546f01cb0fb6ec40f83db62be
jinwoongBang <dlopo123@gmail.com>
2020-12-26 20:36:22 +0900

    [6.1] 단계 쪼개기 - 2단계 리팩토링

--- src/ch6/11_splitPhase/index.js
+++ src/ch6/11_splitPhase/index.js
@@ -4,11 +4,18 @@ function priceOrder(product, quantity, shippingMethod) {
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
+  const price = applyShipping();
+
+  return price;
+}
+
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
   const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountFree
       : shippingMethod.freePerCase;
   const shippingCost = quantity * shippingPerCase;
   const price = basePrice - discount + shippingCost;
+
   return price;
 }

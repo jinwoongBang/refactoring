d44f7fbd245496171b9226730046d71b8160f673
jinwoongBang <dlopo123@gmail.com>
2020-12-27 16:13:06 +0900

    [8.7] 반복문 쪼개기

--- src/ch8/07_splitLoop/index.js
+++ src/ch8/07_splitLoop/index.js
@@ -1,10 +1,13 @@
 function calculateSalary(people) {
   let youngest = people[0] ? people[0].age : Infinity;
-  let totalSalary = 0;
   for (const p of people) {
     if (p.age < youngest) {
       youngest = p.age;
     }
+  }
+
+  let totalSalary = 0;
+  for (const p of people) {
     totalSalary += p.salary;
   }
   return `최연소 : ${youngest}, 총 급여 : ${totalSalary}`;

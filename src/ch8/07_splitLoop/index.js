9d4296c121da3238d92aa884904e4fe530e442ee
jinwoongBang <dlopo123@gmail.com>
2020-12-27 16:14:45 +0900

    [8.7] 함수로 추출

--- src/ch8/07_splitLoop/index.js
+++ src/ch8/07_splitLoop/index.js
@@ -1,14 +1,21 @@
 function calculateSalary(people) {
-  let youngest = people[0] ? people[0].age : Infinity;
-  for (const p of people) {
-    if (p.age < youngest) {
-      youngest = p.age;
+  return `최연소 : ${youngest()}, 총 급여 : ${totalSalary()}`;
+
+  function totalSalary() {
+    let totalSalary = 0;
+    for (const p of people) {
+      totalSalary += p.salary;
     }
+    return totalSalary;
   }
 
-  let totalSalary = 0;
-  for (const p of people) {
-    totalSalary += p.salary;
+  function youngest() {
+    let youngest = people[0] ? people[0].age : Infinity;
+    for (const p of people) {
+      if (p.age < youngest) {
+        youngest = p.age;
+      }
+    }
+    return youngest;
   }
-  return `최연소 : ${youngest}, 총 급여 : ${totalSalary}`;
 }

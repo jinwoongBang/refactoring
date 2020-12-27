c98206f9ea581b5abcbea06b95037004427ed6f6
jinwoongBang <dlopo123@gmail.com>
2020-12-27 17:35:43 +0900

    [8.8] 공백 데이터를 건너 뛰는 로직을 filter 로 변경

--- src/ch8/08_replaceLoopWithPipeline/index.js
+++ src/ch8/08_replaceLoopWithPipeline/index.js
@@ -1,11 +1,8 @@
 export function acquireData(input) {
   const lines = input.split("\n"); // 컬렉션
   const result = [];
-  const loopItem = lines.slice(1);
+  const loopItem = lines.slice(1).filter((line) => line.trim() !== "");
   for (const line of loopItem) {
-    if (line.trim() === "") {
-      continue;
-    }
     const record = line.split(",");
     if (record[1].trim() === "India") {
       result.push({ city: record[0].trim(), phone: record[2].trim() });

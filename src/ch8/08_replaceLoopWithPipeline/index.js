4e245c81ff28ce75f8d568df3db120acb3bd4bc1
jinwoongBang <dlopo123@gmail.com>
2020-12-27 17:37:20 +0900

    [8.8] , 로 구분하는 로직을 map 으로 변경

--- src/ch8/08_replaceLoopWithPipeline/index.js
+++ src/ch8/08_replaceLoopWithPipeline/index.js
@@ -1,9 +1,12 @@
 export function acquireData(input) {
   const lines = input.split("\n"); // 컬렉션
   const result = [];
-  const loopItem = lines.slice(1).filter((line) => line.trim() !== "");
-  for (const line of loopItem) {
-    const record = line.split(",");
+  const loopItems = lines
+    .slice(1)
+    .filter((line) => line.trim() !== "")
+    .map((line) => line.split(","));
+
+  for (const line of loopItems) {
     if (record[1].trim() === "India") {
       result.push({ city: record[0].trim(), phone: record[2].trim() });
     }

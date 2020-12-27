eed03bbbe5d3081fefe6899e318e0029dfe8ffc8
jinwoongBang <dlopo123@gmail.com>
2020-12-27 21:31:19 +0900

    [9.1] 클래스로 추출 및 캡슐화

--- src/ch9/01_splitVariable/index.js
+++ src/ch9/01_splitVariable/index.js
@@ -1,17 +1,46 @@
 function distanceTravelled(scenario, time) {
   let result;
+  
+  const primary = new Primary(scenario, time);
+
   const primaryAcceleration = scenario.primaryForce / scenario.mass; // 가속도 (a) = 힘 (F) / 질량 (m)
-  let primaryTime = Math.min(time, senario.delay);
+  const primaryTime = Math.min(time, senario.delay);
+  const primaryVelocity = primaryAcceleration * scenario.delay;
+
   result = 0.5 * primaryAcceleration * primaryTime * primaryTime; // 전파된 거리
+  
+
   let secondaryTime = time - scenario.delay;
+  const secondaryAcceleration =
+    (scenario.primaryForce + scenario.secondaryForce) / scenario.mass; // 두 번째 힘을 반영해 다시 계산
+
   if (secondaryTime > 0) {
-    // 두 번째 힘을 반영해 다시 계산
-    let primaryVelocity = primaryAcceleration * scenario.delay;
-    const secondaryAcceleration =
-      (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
     result +=
       primaryVelocity * secondaryTime +
       0.5 * secondaryAcceleration * secondaryTime * secondaryTime;
   }
   return result;
+}
+
+class Primary {
+  constructor(scenario, time) {
+    this._scenario = scenario;
+    this._time = time;
+  }
+
+  get acceleration() {
+    return this.scenario.primaryForce / this.scenario.mass;
+  }
+
+  get time() {
+    return Math.min(time, this.senario.delay);
+  }
+
+  get velocity() {
+    return this.acceleration * this.senario.delay;
+  }
+
+  get distance() {
+    return this.acceleration * this.time * this.time;
+  }
 }

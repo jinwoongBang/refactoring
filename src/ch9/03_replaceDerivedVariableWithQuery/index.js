173016eeb6c7d61d191ff00a608bad742a7fafd0
jinwoongBang <dlopo123@gmail.com>
2021-01-25 14:10:18 +0900

    [9-3] 리팩토링 완료

--- src/ch9/03_replaceDerivedVariableWithQuery/index.js
+++ src/ch9/03_replaceDerivedVariableWithQuery/index.js
@@ -1,10 +1,20 @@
+const { assert } = require("chai");
+
 class ProductionPlan {
   get production() {
-    return this._production;
+    // 코드 수정전 테스트를 위한 Assertion
+    // 테스트를 통과 했다면 결과값 변경
+    // assert(this._production === this.calculatedProduction);
+    return this.calculatedProduction;
+  }
+
+  get calculatedProduction() {
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
   }
 
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment);
-    this._production += anAdjustment.amount;
+    // this._production += anAdjustment.amount;
   }
 }
+ 

84a2f5b8c0768a6309736fdb4b91fa23e3753a11
jinwoongBang <dlopo123@gmail.com>
2021-01-26 15:55:45 +0900

    [10-4] function 들을 모두 class의 내부로 로직 이동

--- src/ch10/04_replaceConditionalWithPolymorphism/index2.js
+++ src/ch10/04_replaceConditionalWithPolymorphism/index2.js
@@ -1,84 +1,108 @@
+// [Client]
+const voyage = { zone: "서인도", length: 10 };
+const history = [
+  { zone: "동인도", profit: 5 },
+  { zone: "서인도", profit: 15 },
+  { zone: "중국", profit: -2 },
+  { zone: "서아프키ㅏ", profit: 7 },
+];
+
+const myRating = rating(voyage, history);
+
+// [Back-end]
+
 // 투자 등급
-function rating() {
-  const vpf = voyageProfitFactor(voyage, history);
-  const vr = voyageRisk(voyage);
-  const chr = captainHistoryRisk(voyage, history);
-
-  if (vpf * 3 > vr + chr * 2) {
-    return "A";
-  } else {
-    return "B";
-  }
+function rating(voyage, history) {
+  return new Rating(voyage, history).value;
 }
 
-// 항해 경로 위험요소
-function voyageRisk() {
-  let result = 1;
-
-  if (voyage.length > 4) {
-    result += 2;
+class Rating {
+  constructor(voyage, history) {
+    this.voyage = voyage;
+    this.history = history;
   }
 
-  if (voyage.length > 8) {
-    result += voyage.length - 8;
+  get value() {
+    const vpf = this.voyageProfitFactor(voyage, history);
+    const vr = this.voyageRisk(voyage);
+    const chr = this.captainHistoryRisk(voyage, history);
+
+    if (vpf * 3 > vr + chr * 2) {
+      return "A";
+    } else {
+      return "B";
+    }
   }
 
-  if (["중국", "동인도"].includes(voyage.zone)) {
-    result += 4;
+  // 항해 경로 위험요소
+  get voyageRisk() {
+    let result = 1;
+
+    if (this.voyage.length > 4) {
+      result += 2;
+    }
+
+    if (this.voyage.length > 8) {
+      result += this.voyage.length - 8;
+    }
+
+    if (["중국", "동인도"].includes(this.voyage.zone)) {
+      result += 4;
+    }
+
+    return Math.max(result, 0);
   }
 
-  return Math.max(result, 0);
-}
+  // 선장의 항해 이력 위험요소
+  get captainHistoryRisk() {
+    let result = 1;
+    if (this.history.length < 5) {
+      result += 4;
+    }
 
-// 선장의 항해 이력 위험요소
-function captainHistoryRisk(voyage, history) {
-  let result = 1;
-  if (history.length < 5) {
-    result += 4;
+    result += this.history.filter((v) => v.profit < 0).length;
+
+    if (this.voyage.zone === "중국" && this.hasChinaHistory) {
+      result -= 2;
+    }
+
+    return Math.max(result, 0);
   }
 
-  result += history.filter((v) => v.profit < 0).length;
-
-  if (voyage.zone === "중국" && hasChina(history)) {
-    result -= 2;
-  }
-
-  return Math.max(result, 0);
-}
-
-// 중국을 경유하는가?
-function hasChina(history) {
-  return history.some((v) => "중국" === v.zone);
-}
-
-// 수익 요인
-function voyageProfitFactor(voyage, history) {
-  let result = 2;
-  if (voyage.zone === "중국") {
-    result += 1;
-  }
-  if (voyage.zone === "동인도") {
-    result += 1;
-  }
-  if (voyage.zone === "중국" && hasChina(history)) {
-    result += 3;
-    if (history.length > 10) {
+  // 수익 요인
+  get voyageProfitFactor() {
+    let result = 2;
+    if (this.voyage.zone === "중국") {
       result += 1;
     }
-    if (voyage.length > 12) {
+    if (this.voyage.zone === "동인도") {
       result += 1;
     }
-    if (voyage.length > 18) {
-      result -= 1;
+    if (this.voyage.zone === "중국" && this.hasChinaHistory) {
+      result += 3;
+      if (this.history.length > 10) {
+        result += 1;
+      }
+      if (this.voyage.length > 12) {
+        result += 1;
+      }
+      if (this.voyage.length > 18) {
+        result -= 1;
+      }
+    } else {
+      if (this.history.length > 8) {
+        result += 1;
+      }
+      if (this.voyage.length > 14) {
+        result -= 1;
+      }
     }
-  } else {
-    if (history.length > 8) {
-      result += 1;
-    }
-    if (voyage.length > 14) {
-      result -= 1;
-    }
+
+    return result;
   }
 
-  return result;
+  // 중국을 경유하는가?
+  get hasChinaHistory() {
+    return this.history.some((v) => "중국" === v.zone);
+  }
 }

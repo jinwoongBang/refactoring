cf92d390c17b29786ea69c4691c2ec9cfb034d6d
jinwoongBang <dlopo123@gmail.com>
2021-01-26 14:22:07 +0900

    [10-4] Bird 를 상속 받는 새의 여러 종류 클래스 생성 및 새를 생성하는 factory 클래스 작성

--- src/ch10/04_replaceConditionalWithPolymorphism/index.js
+++ src/ch10/04_replaceConditionalWithPolymorphism/index.js
@@ -3,6 +3,10 @@
  * @summary {
  *  새의 종에 따른 비행 속도와 깃털 상태를 알려주는 로직
  * }
+ * @description {
+ *  아카이브 상태, 리스토어 상태 등 상태에 대한 로직을 이렇게 해도 괜찮을 듯
+ *  그리고 Bird 클래스를 상속 받는 객체들 처럼 KeyFlowVO 객체도 Object.assign 으로 복사에서 객체를 new 생성해도 될것 같은데?????
+ * }
  */
 
 function plumages(birds) {
@@ -21,6 +25,19 @@ function plumage(bird) {
 // 비행 속도
 function airSpeedVelocity(bird) {
   return new Bird(bird).airSpeedVelocity;
+}
+
+function createBird(bird) {
+  switch (bird.type) {
+    case "유럽 제비":
+      return new EuropeanSwallow(bird);
+    case "아프리카 제비":
+      return 40 - 2 * this.numberOfCoconuts;
+    case "노르웨이 파랑 앵무":
+      return this.isNailed ? 0 : 10 + this.voltage / 10;
+    default:
+      return null;
+  }
 }
 
 class Bird {
@@ -54,3 +71,7 @@ class Bird {
     }
   }
 }
+
+class EuropeanSwallow extends Bird {}
+class AfricanSwallow extends Bird {}
+class NorwegianBlueParrot extends Bird {}

ac67fc50869546b52a07aa4da9b1014117b4dede
jinwoongBang <dlopo123@gmail.com>
2021-01-26 13:29:51 +0900

    [10-3] result 변수의 값을 변경하는 것이 아닌 바로

--- src/ch10/03_replaceNestedConditionalWithGuardClauses/index.js
+++ src/ch10/03_replaceNestedConditionalWithGuardClauses/index.js
@@ -6,20 +6,23 @@ function payAmount(employee) {
   let result;
   if (employee.isSeparated) {
     // 퇴사한 직원인가?
-    result = { amount: 0, reasonCode: "SEP" };
-  } else {
-    if (employee.isRetired) {
-      // 은퇴한 직원인가?
-      result = { amount: 0, reasonCode: "RET" };
-    } else {
-      // 급여 계산 로직
-      lorem.ipsum(dolor.sitAmet);
-      consectetur(adipiscing).elit();
-      setImmediate.do.eiusmod =
-        tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-    }
+    // result = { amount: 0, reasonCode: "SEP" };
+    return { amount: 0, reasonCode: "SEP" };
   }
+
+  if (employee.isRetired) {
+    // 은퇴한 직원인가?
+    //   result = { amount: 0, reasonCode: "RET" };
+    return { amount: 0, reasonCode: "RET" };
+  }
+
+  // 급여 계산 로직
+  lorem.ipsum(dolor.sitAmet);
+  consectetur(adipiscing).elit();
+  setImmediate.do.eiusmod =
+    tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+  ut.enim.ad(minim.veniam);
+  result = someFinalComputation();
+
   return result;
 }

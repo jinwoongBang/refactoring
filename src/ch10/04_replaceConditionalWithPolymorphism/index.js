bc67832cb11962fc4d13229287c7c93258aacb56
jinwoongBang <dlopo123@gmail.com>
2021-01-26 14:43:23 +0900

    [10-4] factory 매소드 및 다형성을 이용하도록 리팩토링 완료

--- src/ch10/04_replaceConditionalWithPolymorphism/index.js
+++ src/ch10/04_replaceConditionalWithPolymorphism/index.js
@@ -9,22 +9,16 @@
  * }
  */
 
-function plumages(birds) {
-  return new Map(birds.map((b) => [b.name, plumage(b)]));
-}
-
-function speeds(birds) {
-  return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
-}
-
 // 깃털 상태
-function plumage(bird) {
-  return createBird(bird).plumage;
+function plumages(birds) {
+//   return new Map(birds.map((b) => [b.name, plumage(b)]));
+  return new Map(birds.map((b) => createBird(b)).map((bird) => [bird.name, bird.plumage]));
 }
 
 // 비행 속도
-function airSpeedVelocity(bird) {
-  return createBird(bird).airSpeedVelocity;
+function speeds(birds) {
+//   return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
+return new Map(birds.map((b) => createBird(b)).map((bird) => [bird.name, bird.airSpeedVelocity]));
 }
 
 function createBird(bird) {
@@ -46,32 +40,34 @@ class Bird {
   }
 
   get plumage() {
-    switch (this.type) {
-      case "유럽 제비":
-        return "보통이다";
-      case "아프리카 제비":
-        return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
-      case "노르웨이 파랑 앵무":
-        return this.voltage > 100 ? "그을렸다" : "예쁘다";
-      default:
-        return "알 수 없다";
-    }
+    return "알 수 없다";
   }
 
   get airSpeedVelocity() {
-    switch (this.type) {
-      case "유럽 제비":
-        return 35;
-      case "아프리카 제비":
-        return 40 - 2 * this.numberOfCoconuts;
-      case "노르웨이 파랑 앵무":
-        return this.isNailed ? 0 : 10 + this.voltage / 10;
-      default:
-        return null;
-    }
+   return null;
   }
 }
 
-class EuropeanSwallow extends Bird {}
-class AfricanSwallow extends Bird {}
-class NorwegianBlueParrot extends Bird {}
+class EuropeanSwallow extends Bird {
+  get plumage() {
+    return "보통이다";
+  }
+  get airSpeedVelocity() {
+    return 35;
+  }
+}
+class AfricanSwallow extends Bird {
+  get plumage() {
+    return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
+  }
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+class NorwegianBlueParrot extends Bird {
+  get plumage() {
+    return this.voltage > 100 ? "그을렸다" : "예쁘다";
+  }
+  get airSpeedVelocity() {
+    return this.isNailed ? 0 : 10 + this.voltage / 10;
+}

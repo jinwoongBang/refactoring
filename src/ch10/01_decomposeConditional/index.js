f4505e88c89d3787e745348ba368fd0f8139ec90
jinwoongBang <dlopo123@gmail.com>
2021-01-26 11:22:21 +0900

    [10-1] 할인율 계산 로직 함수로 추출

--- src/ch10/01_decomposeConditional/index.js
+++ src/ch10/01_decomposeConditional/index.js
@@ -1,11 +1,15 @@
 let charge;
 
 if (summer()) {
-  charge = quantity * plan.summerRate;
+  charge = summerCharge();
 } else {
   charge = quantity * plan.regularRate + plan.regularServiceCharge;
 }
 
 function summer() {
   return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+}
+
+function summerCharge() {
+  return quantity * plan.summerRate;
 }

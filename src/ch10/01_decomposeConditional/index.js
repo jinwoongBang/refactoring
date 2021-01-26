b5315543dd8dfb6205f156b2896fc1a0fe6442fe
jinwoongBang <dlopo123@gmail.com>
2021-01-26 11:20:58 +0900

    [10-1] 조건을 함수로 추출하기

--- src/ch10/01_decomposeConditional/index.js
+++ src/ch10/01_decomposeConditional/index.js
@@ -1,7 +1,11 @@
 let charge;
 
-if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd)) {
+if (summer()) {
   charge = quantity * plan.summerRate;
 } else {
   charge = quantity * plan.regularRate + plan.regularServiceCharge;
+}
+
+function summer() {
+  return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
 }

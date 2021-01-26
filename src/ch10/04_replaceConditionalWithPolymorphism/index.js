abb4bd9728712e25766aee2ff49d8ae8379ed446
jinwoongBang <dlopo123@gmail.com>
2021-01-26 14:12:17 +0900

    [10-4] Bird 새 클래스를 만들고 switch 구문을 클래스에서 처리

--- src/ch10/04_replaceConditionalWithPolymorphism/index.js
+++ src/ch10/04_replaceConditionalWithPolymorphism/index.js
@@ -15,16 +15,7 @@ function speeds(birds) {
 
 // 깃털 상태
 function plumage(bird) {
-  switch (bird.type) {
-    case "유럽 제비":
-      return "보통이다";
-    case "아프리카 제비":
-      return bird.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
-    case "노르웨이 파랑 앵무":
-      return bird.voltage > 100 ? "그을렸다" : "예쁘다";
-    default:
-      return "알 수 없다";
-  }
+  return new Bird(bird).plumage;
 }
 
 // 비행 속도
@@ -38,5 +29,24 @@ function airSpeedVelocity(bird) {
       return bird.isNailed ? 0 : 10 + bird.voltage / 10;
     default:
       return null;
+  }
+}
+
+class Bird {
+  constructor(birdObject) {
+    Object.assign(this, birdObject);
+  }
+
+  get plumage() {
+    switch (this.type) {
+      case "유럽 제비":
+        return "보통이다";
+      case "아프리카 제비":
+        return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
+      case "노르웨이 파랑 앵무":
+        return this.voltage > 100 ? "그을렸다" : "예쁘다";
+      default:
+        return "알 수 없다";
+    }
   }
 }

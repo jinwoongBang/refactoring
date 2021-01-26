5760c3a2e6bc7219a5e06b4869c4a9865bef4546
jinwoongBang <dlopo123@gmail.com>
2021-01-26 11:49:12 +0900

    [10-2] 조건문 함수로 추출

--- src/ch10/02_consolidateConditionalExpression/index.js
+++ src/ch10/02_consolidateConditionalExpression/index.js
@@ -3,12 +3,16 @@
  */
 
 function disablityAmount(anEmployee) {
-  if (
-    anEmployee.seniority < 2 ||
-    anEmployee.monthsDisabled > 12 ||
-    anEmployee.isPartTime
-  ) {
+  if (isNotEligibleForDisability()) {
     return 0;
   }
   // TODO: 장애 수당 계산
+}
+
+function isNotEligibleForDisability() {
+  return (
+    anEmployee.seniority < 2 ||
+    anEmployee.monthsDisabled > 12 ||
+    anEmployee.isPartTime
+  );
 }
